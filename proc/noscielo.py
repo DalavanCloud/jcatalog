edd3fe3607ef180755cbfa78a509ea5d06d79741
ednilson <edy.gesseff@gmail.com>
2017-06-01 16:36:40 -0300

    Revisao de periódicos "não SciELO"

--- proc/noscielo.py
+++ proc/noscielo.py
@@ -3,78 +3,82 @@
 import models
 
 
-models.Noscielo.drop_collection()
+models.Noscielo2.drop_collection()
 
+
+# 1) Scopus - first load in Noscielo2
 print('Scopus Not SciELO: %s' % models.Scopus.objects.filter(publishers_country='Brazil', is_scielo=0, source_type='Journal').count()) #185
+
+
 for doc in models.Scopus.objects.filter(publishers_country='Brazil', is_scielo=0, source_type='Journal'):
+
     noscielo = {}
+
     noscielo['issn_list'] = doc.issn_list
     noscielo['title'] = doc.source_title
     noscielo['scopus_id'] = str(doc.id)
     noscielo['is_scopus'] = doc.is_scopus
-    mdata = models.Noscielo(**noscielo)
+    
+    if hasattr(doc, 'scimago_id'):
+        noscielo['scimago_id'] = str(doc.scimago_id)
+        noscielo['is_scimago'] = doc.is_scimago
+    
+    if hasattr(doc, 'wos_id'):
+        noscielo['wos_id'] = doc.wos_id
+        noscielo['is_wos'] = doc.is_wos
+    
+    mdata = models.Noscielo2(**noscielo)
     mdata.save()
 
+
+# 2) Scimago - load of Scimago in Noscielo2
 print('Scimago Not SciELO: %s' % models.Scimago.objects.filter(country='Brazil', is_scielo=0).count()) #140
-for doci in models.Noscielo.objects():
-    for issn in doci.issn_list:
-        result = models.Scimago.objects.filter(country='Brazil', is_scielo=0, issn_list=issn)
-        print(issn)
-        if result:
-            for r in result:
-                doci.modify(
-                    is_scimago = 1,
-                    scimago_id = str(r.id))
-                doci.save()
-                pass
+
+docs = models.Scimago.objects.filter(country='Brazil', is_scielo=0)
+
+for doc in docs:
+
+    if hasattr(doc, 'is_scopus'): # Scimago journals present in Scopus
+        pass
+    else:
+        noscielo = {}
+
+        noscielo['issn_list'] = doc.issn_list
+        noscielo['title'] = doc.title
+        noscielo['scimago_id'] = str(doc.id)
+        noscielo['is_scimago'] = doc.is_scimago
+        
+        if hasattr(doc, 'wos_id'):
+            noscielo['wos_id'] = doc.wos_id
+            noscielo['is_wos'] = 1
+        
+        mdata = models.Noscielo2(**noscielo)
+        mdata.save()
 
 
-    tresult = models.Scimago.objects.filter(country='Brazil', is_scielo=0, title__iexact=doci.title)
-    if tresult:
-        for t in tresult:
-            doci.modify(
-                is_scimago = 1, 
-                scimago_id = str(t.id))
-            doci.save()
-            pass
-
-for doci in models.Noscielo.objects():
-    if not hasattr(doci, 'is_scimago'):
-        doci.modify(is_scimago=0)
-        doci.save()
-
-
-print('----------------------------------------')
+# 3) WoS - load of WoS in Noscielo2
 print('Wos Not SciELO: %s' % models.Wos.objects.filter(is_scielo=0).count()) #22
-for docw in models.Noscielo.objects():
-    for issn in docw.issn_list:
-        resultw = models.Wos.objects.filter(is_scielo=0, issn_list=issn)
-        print(issn)
-        if resultw:
-            for r in resultw:
-                docw.modify(
-                    is_wos = 1,
-                    wos_id = str(r.id) 
-                    )
-                docw.save()
-                pass
 
-    tresult = models.Wos.objects.filter(is_scielo=0, full_journal_title__iexact=docw.title)
-    if tresult:
-        for tr in tresult:
-            print('wos:'+tr.full_journal_title)
-            print('isw:'+str(tr.is_wos))
-            docw.modify(
-                is_wos = 1, 
-                wos_id = str(tr.id))
-            docw.save()
-            pass
-    
-    print(models.Noscielo.objects.get(id=str(docw.id)).to_json())
-    print('\n')
+docs = models.Wos.objects.filter(country='Brazil', is_scielo=0)
+
+for doc in docs:
+
+    if hasattr(doc, 'is_scopus') or hasattr(doc, 'is_scimago'): # WoS journals present in Scopus or Scimago
+        pass
+    else:
+        noscielo = {}
+
+        noscielo['issn_list'] = doc.issn_list
+        noscielo['title'] = doc.title
+        noscielo['wos_id'] = str(doc.id)
+        noscielo['is_wos'] = doc.is_wos
+        
+        mdata = models.Noscielo2(**noscielo)
+        mdata.save()
 
 
-for docw in models.Noscielo.objects():
-    if not hasattr(docw, 'is_wos'):
-        docw.modify(is_wos=0)
-        docw.save()
+# 4) Complete whith 0 documents that do not have the attribute 'is_wos'
+for doc in models.Noscielo2.objects():
+    if not hasattr(doc, 'is_wos'):
+        doc.modify(is_wos=0)
+        doc.save()
