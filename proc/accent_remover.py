fbe64675c5118e6c0e03075523f83f654a658bea
ednilson <edy.gesseff@gmail.com>
2017-06-13 16:41:20 -0300

    Criar campo com título e país sem acentos e caixa baixa - tk #84

---
+++ proc/accent_remover.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import unicodedata
+
+
+def accent_remover(text):
+        
+    norm = unicodedata.normalize('NFKD', text)
+
+    text_norm = u''.join([c for c in norm if not unicodedata.combining(c)])
+
+    return text_norm
