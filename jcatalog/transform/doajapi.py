38e8ae48ad75503dfd29c2267e5149165b8c3da1
ednilson <edy.gesseff@gmail.com>
2017-09-04 15:15:43 -0300

    Adicionado periodicos brasileiros e nao SciELO

--- jcatalog/transform/doajapi.py
+++ jcatalog/transform/doajapi.py
@@ -10,37 +10,43 @@ import models
 logging.basicConfig(filename='logs/doajapi.info.txt', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 models.Doajapi.drop_collection()
 
 url = 'https://doaj.org/api/v1/search/journals/issn:'
 
-for doc in models.Scielo.objects():
+for dbcol in (
+    models.Scielo.objects,
+    models.Noscielo.objects
+        ):
 
-    flag = 0
+    for doc in dbcol:
+        print(dbcol._collection.name)
 
-    for issn in doc.issn_list:
+        flag = 0
+        for issn in doc['issn_list']:
 
-        if flag == 0:
+            if flag == 0:
 
-            r = requests.get(url + '%s' % issn)
+                r = requests.get(url + '%s' % issn)
 
-            docdoaj = r.json()
+                docdoaj = r.json()
 
-            if docdoaj['total'] > 0:
-                docdoaj['issn_list'] = [issn]
-                docdoaj['scielo_id'] = str(doc.id)
-                docdoaj['title'] = docdoaj.results[0]['bibjson']['title']
-                mdata = models.Doajapi(**docdoaj)
-                mdata.save()
+                if docdoaj['total'] > 0:
+                    docdoaj['issn_list'] = [issn]
+                    docdoaj['scielo_id'] = str(doc.id)
+                    docdoaj['title'] = docdoaj['results'][0]['bibjson']['title']
+                    mdata = models.Doajapi(**docdoaj)
+                    mdata.save()
 
-                msg = 'ISSN: %s found' % (issn)
-                logger.info(msg)
-                print(msg)
+                    msg = 'ISSN: %s found' % (issn)
+                    logger.info(msg)
+                    print(msg)
 
-                flag = 1
-                break
+                    flag = 1
+                    break
 
-            else:
-                msg = 'ISSN: %s not found' % (issn)
-                logger.info(msg)
-                print(msg)
+                else:
+                    msg = 'ISSN: %s not found' % (issn)
+                    logger.info(msg)
+                    print(msg)
