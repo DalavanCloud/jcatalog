1564cfb5efe713c5987d7fbad5f7f794d5d04bf4
ednilson <edy.gesseff@gmail.com>
2018-07-11 08:12:19 -0300

    Otimiza codigo - lista de emails

--- jcatalog/reports/list_send.py
+++ jcatalog/reports/list_send.py
@@ -3,52 +3,28 @@ import models
 import xlsxwriter
 
 workbook = xlsxwriter.Workbook('output/lista_emails_avalia_scielo_fapesp.xlsx')
-worksheet = workbook.add_worksheet('lista_emails_editores')
+worksheet = workbook.add_worksheet('emails_editores')
 
-query = models.Scielo.objects.filter(
-    title_current_status='current',
-    collection='scl')
+# Filtras SciELO Brasil, current, entrada no scielo antes de 2016
+query = models.Scielofapesp.objects.filter(fapesp_evaluation__2018__evaluated=1)
+
 counter = 0
 
 row = 0
 
-# Filtras SciELO Brasil, current, entrada no scielo antes de 2016
 for j in query:
-    docs16 = 0
-    entrada = 0
-    ativo_y = 0
 
-    if 'docs' in j:
-        if j['docs']['docs_2016'] == '':
-            pass
-        elif j['docs']['docs_2016'] > 0:
-            docs16 = 1
+    worksheet.write(row, 0, j['issn_scielo'])
+    worksheet.write(row, 1, j['title'])
 
-        for year in range(2007, 2019):
-            y = str(year)
-            if 'docs_'+y in j['docs']:
-                if j['docs']['docs_'+y] == '':
-                    pass
-                elif int(j['docs']['docs_'+y]) > 0:
-                    ativo_y = 1
-                    break
+    if 'avaliacao' in j:
+        if 'contatos' in j['avaliacao']:
+            worksheet.write(row, 2, ','.join([e['email_address'] for e in j['avaliacao']['contatos']]))
+    else:
+        print(j['issn_scielo'] + "sem email")
 
-    if j['inclusion_year_at_scielo'] < 2016:
-        entrada = 1
+    worksheet.write(row, 3, 'http://static.scielo.org/fapesp_evaluation/avaliacao_scielo_'+j['issn_scielo']+'.xlsx')
 
-    if docs16 == 1 and entrada == 1 and ativo_y == 1:
-
-        worksheet.write(row, 0, j['issn_scielo'])
-        worksheet.write(row, 1, j['title'])
-
-        if 'avaliacao' in j:
-            if 'contatos' in j['avaliacao']:
-                worksheet.write(row, 2, ','.join([e['email_address'] for e in j['avaliacao']['contatos']]))
-        else:
-            print(j['issn_scielo'] + "sem email")
-
-        worksheet.write(row, 3, 'http://static.scielo.org/fapesp_evaluation/avaliacao_scielo_'+j['issn_scielo']+'.xlsx')
-
-        row += 1
+    row += 1
 
 workbook.close()
