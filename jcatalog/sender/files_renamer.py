d5d3a49518a3596fe9c33bcaba6393b47b18be2d
ednilson <edy.gesseff@gmail.com>
2018-07-24 16:43:20 -0300

    Refeito o renomeador para PDFs

--- jcatalog/sender/files_renamer.py
+++ jcatalog/sender/files_renamer.py
@@ -5,7 +5,7 @@ import models
 from shutil import copyfile
 
 
-def renamer(filename):
+def renamer_docx(filename):
     sheet = pyexcel.get_sheet(
         file_name=filename,
         sheet_name='toform',
@@ -37,9 +37,31 @@ def renamer(filename):
                 copyfile(origem, 'renomeado/' + destino)
 
 
+def renamer_pdf(filename):
+    sheet = pyexcel.get_sheet(
+        file_name=filename,
+        sheet_name='toform',
+        name_columns_by_row=0)
+
+    sheet_json = sheet.to_records()
+
+    os.chdir('output/journals/forms/pdfs')
+
+    for l in sheet_json:
+        for f in os.listdir('.'):
+            if str(l['file_name']) + '.pdf' == f:
+                print(str(l['file_name']) + '.pdf --> ' +
+                      'Formulario-avaliacao-Fapesp-SciELO-' + l['issn'] + '-20180713.pdf')
+                copyfile(f, 'renomeado/Formulario-avaliacao-Fapesp-SciELO-' +
+                         l['issn'] + '-20180713.pdf')
+
+
 def main():
     filename = 'data/scielo/data_to_form_texto_20180720.xlsx'
-    renamer(filename)
+
+    # renamer_docx(filename)
+
+    renamer_pdf(filename)
 
 if __name__ == "__main__":
     main()
