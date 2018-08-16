5dcee1a41561cf3259c2bac908dddc80e6a61717
ednilson <edy.gesseff@gmail.com>
2018-08-16 11:40:40 -0300

    Atualiza dados Pubmed API

--- jcatalog/transform/pubmedapi.py
+++ jcatalog/transform/pubmedapi.py
@@ -5,67 +5,74 @@ https://www.ncbi.nlm.nih.gov/books/NBK25500/
 '''
 import logging
 import requests
+import time
 
 import models
 
 logging.basicConfig(filename='logs/pubmedapi.info.txt', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-models.Pubmedapi.drop_collection()
+# Drop Collection
+# models.Pubmedapi.drop_collection()
 
 url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
 
-for dbcol in (models.Scielo.objects, models.Noscielo.objects):
+query = models.Scielo.objects().batch_size(5)
 
-    for doc in dbcol():
-        print(dbcol._collection.name)
+for doc in query[1215:]:
 
-        flag = 0
+    flag = 0
 
-        for issn in doc['issn_list']:
+    for issn in doc['issn_list']:
 
-            if flag == 0:
+        if flag == 0:
 
-                data = {}
+            data = {}
 
-                # pubmed
-                r = requests.get('%s?db=%s&term=%s[journal]&retmode=json' % (url, 'pubmed', issn))
+            # pubmed
+            r = requests.get(
+                '%s?db=%s&term=%s[journal]&retmode=json' % (url, 'pubmed', issn))
 
-                docpubmed = r.json()
+            docpubmed = r.json()
 
+            if 'esearchresult' in docpubmed:
                 if int(docpubmed['esearchresult']['count']) > 0:
                     data['db_name'] = ['pubmed']
-                    data['pubmed_count'] = int(docpubmed['esearchresult']['count'])
+                    data['pubmed_count'] = int(
+                        docpubmed['esearchresult']['count'])
 
-                # pmc
-                r = requests.get('%s?db=%s&term=%s[journal]&retmode=json' % (url, 'pmc', issn))
+            # time.sleep(1)
 
-                docpmc = r.json()
+            # pmc
+            r = requests.get(
+                '%s?db=%s&term=%s[journal]&retmode=json' % (url, 'pmc', issn))
 
+            docpmc = r.json()
+
+            if 'esearchresult' in docpubmed:
                 if int(docpmc['esearchresult']['count']) > 0:
                     if 'pmc' not in data['db_name']:
                         data['db_name'].append('pmc')
                     data['pmc_count'] = int(docpmc['esearchresult']['count'])
 
-                # scielo
-                if 'db_name' in data:
-                    data['issn_list'] = [issn]
-                    data['scielo_id'] = str(doc.id)
-                    data['db_col'] = dbcol._collection.name
-                    if dbcol._collection.name == 'scielo':
-                        data['title_country'] = doc.title_country
+            # scielo
+            if 'db_name' in data:
+                data['issn_list'] = [issn]
+                data['scielo_id'] = str(doc.id)
+                data['db_col'] = doc['collection']
+                data['title_country'] = doc.title_country
 
-                # save data
-                if 'db_name' in data:
-                    mdata = models.Pubmedapi(**data)
-                    mdata.save()
+            # save data
+            if 'db_name' in data:
+                mdata = models.Pubmedapi(**data)
+                mdata.save()
 
-                    msg = 'ISSN: %s found' % (issn)
-                    logger.info(msg)
-                    print(msg)
+                msg = 'ISSN: %s found' % (issn)
+                logger.info(msg)
+                print(msg)
 
-                    flag = 1
-                else:
-                    msg = 'ISSN: %s not found' % (issn)
-                    logger.info(msg)
-                    print(msg)
+                flag = 1
+            else:
+                msg = 'ISSN: %s not found' % (issn)
+                logger.info(msg)
+                print(msg)
